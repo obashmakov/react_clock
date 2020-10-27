264248f3bae6b61aa248c74a77445348a15dd4eb
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-10-27 16:02:30 +0200

    Updates

---
+++ precache-manifest.3c0bf4db11413861a69c747dc8e7dfaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0eff290b2db75649a01230ff4795824",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7961e498d045debf67c5",
+    "url": "./static/css/main.29533464.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7961e498d045debf67c5",
+    "url": "./static/js/main.13d71c1c.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
